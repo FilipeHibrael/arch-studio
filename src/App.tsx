b6916772fae52526be5207fd08696c7d8d39636b
FilipeHibrael <filipehibrael27@gmail.com>
2024-01-22 19:27:04 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 export default function App() {
   return (
     <div className="max-w-[1440px] mx-auto px-40">
       <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
