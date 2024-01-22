067f1056aa0aef6f33221e6da1ffe4bc28a2cfbe
FilipeHibrael <filipehibrael27@gmail.com>
2024-01-22 20:41:16 -0300

    dataContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import Header from './components/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import { DataContextProvider } from './context/DataContext';
 
 export default function App() {
   return (
     <div className="max-w-[1440px] mx-auto px-40">
       <Header />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <DataContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </DataContextProvider>
       </BrowserRouter>
     </div>
   );
---
+++ src/context/DataContext.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+
+type IImage = {
+  desktop: string;
+  tablet: string;
+  mobile: string;
+};
+
+type ISlide = {
+  title: string;
+  content: string;
+  images: IImage;
+};
+
+type IHomePageWelcome = {
+  img: string;
+  heading: string;
+  paragraphs: {
+    one: string;
+    two: string;
+    three: string;
+  };
+};
+
+type IHero = {
+  image: IImage;
+  headingLarge: string;
+  heading: string;
+  paragraph: string;
+};
+
+type IHeritage = {
+  heritageImg: string;
+  heading: string;
+  paragraphs: {
+    one: string;
+    two: string;
+    three: string;
+  };
+};
+
+type ILeaderInfo = {
+  name: string;
+  role: string;
+  img: string;
+};
+
+type IAboutData = {
+  hero: IHero;
+  heritage: IHeritage;
+  leaders: {
+    heading: string;
+    leadersInfo: ILeaderInfo[];
+  };
+};
+
+type IOffice = {
+  name: string;
+  mail: string;
+  address: string;
+  phone: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+};
+
+type IDefaultMap = {
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+  zoom: number;
+};
+
+type ISectionDetailsAndMap = {
+  offices: IOffice[];
+  defaultMap: IDefaultMap;
+};
+
+type IContactData = {
+  hero: IHero;
+  sectionDetailsAndMap: ISectionDetailsAndMap;
+};
+
+type IPortfolioItem = {
+  id: number;
+  title: string;
+  date: string;
+  image: IImage;
+};
+
+type IData = {
+  slider: ISlide[];
+  HomePageWelcome: IHomePageWelcome;
+  HomePageBannerImgs: IImage;
+  aboutData: IAboutData;
+  contactData: IContactData;
+  portfolioData: IPortfolioItem[];
+};
+
+type IDataContext = {
+  data: IData | null;
+  loading: boolean;
+  error: string | null;
+};
+
+const DataContext = React.createContext<IDataContext | null>(null);
+
+export const useData = () => {
+  const context = React.useContext(DataContext);
+  if (!context) throw new Error('useData precisa estar em DataContextProvider');
+  return context;
+};
+
+export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const { data, loading, error } = useFetch<IData>('./public/data.json');
+
+  return (
+    <DataContext.Provider value={{ data, loading, error }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
